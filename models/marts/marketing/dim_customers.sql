1f4df8f0757ab1b22f3b9101d83c79a411b1d1f7
Danny Khant <phyominnthwin@gmail.com>
2026-03-19 04:42:27 +0000

    feat: added lifetime_value in dim_customers

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -6,13 +6,19 @@ orders as (
     select * from {{ ref('stg_jaffle_shop__orders') }}
 ),
 
+payments as (
+    select * from {{ ref('stg_stripe__payments') }}
+),
+
 customer_orders as (
     select
-        customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as last_order_date,
-        count(order_id) as number_of_orders
-    from orders
+        o.customer_id,
+        min(o.order_date) as first_order_date,
+        max(o.order_date) as last_order_date,
+        count(o.order_id) as number_of_orders,
+        sum(p.amount) as lifetime_value
+    from orders as o
+    join payments as p
     group by 1
 ),
 
@@ -23,7 +29,8 @@ final as (
         c.last_name,
         co.first_order_date,
         co.last_order_date,
-        coalesce(co.number_of_orders, 0) as number_of_orders
+        coalesce(co.number_of_orders, 0) as number_of_orders,
+        coalesce(co.lifetime_value, 0) as lifetime_value
     from customers as c
     left join customer_orders co
     using (customer_id)
